a23905dff553eba13f230751b2f84737f4a540dc
fatekkl <mathtml.1105@gmail.com>
2025-02-17 10:47:54 -0300

    call new menu + remove image in wrong location

--- src/Components/Authenticated/index.tsx
+++ src/Components/Authenticated/index.tsx
@@ -1,28 +1,28 @@
-import HistoryItem from "../HistoryItem"
-import StreakDisplayer from "../StreakDisplayer"
+import HistoryItem from "../HistoryItem";
+import Menu from "../Menu";
+import StreakDisplayer from "../StreakDisplayer";
 
 function Authenticated() {
     return (
         <section className="w-screen h-screen bg-tn_white flex flex-col justify-between items-center">
-            {/* Container principal com alinhamento correto */}
-            <div className="w-full h-full flex items-center justify-center px-20 mt-20">
+            <Menu />
 
-                {/* Retângulo marrom à esquerda */}
-                <div className="w-60 h-20 bg-tn_brown" />
-
+            {/* Container principal */}
+            <div className="w-full h-full flex items-center justify-center px-20 mt-20 relative">
+                
                 {/* Logo e texto centralizados */}
-                <div className="flex flex-col items-center justify-center gap-6 flex-1">
+                <div className="flex flex-col items-center justify-center gap-6 absolute left-1/2 transform -translate-x-1/2">
                     <img src="/assets/thenews_logo.png" className="w-60 object-contain" />
                     <p className="text-tn_yellow font-bold text-5xl text-center">Veja sua sequência</p>
                 </div>
 
-                {/* Streak Displayer centralizado */}
-                <div className="w-60 flex items-center justify-center">
+                {/* Streak Displayer posicionado à direita */}
+                <div className="absolute right-20">
                     <StreakDisplayer />
                 </div>
             </div>
 
-            {/* Barra inferior marrom centralizada */}
+            {/* Histórico centralizado na parte inferior */}
             <div className="h-full w-96 mb-8 p-4 flex flex-col gap-6 overflow-auto">
                 <HistoryItem content={"15/02/2025"} />
                 <HistoryItem content={"14/02/2025"} />
@@ -35,14 +35,14 @@ function Authenticated() {
                 <HistoryItem content={"13/02/2025"} />
             </div>
 
-            {/* Texto motivacional na parte inferior */}
+            {/* Texto motivacional fixado na parte inferior */}
             <div className="h-1/3 w-screen bg-tn_yellow flex items-center justify-center">
                 <h1 className="text-tn_brown font-bold text-center text-3xl">
                     Sua sequência está forte como um expresso!
                 </h1>
             </div>
         </section>
-    )
+    );
 }
 
-export default Authenticated
+export default Authenticated;
