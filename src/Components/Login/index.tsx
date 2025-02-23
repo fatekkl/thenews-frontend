8e43352bf39427e1b2fdc65a63aefd6d10c43d0b
fatekkl <mathtml.1105@gmail.com>
2025-02-23 12:09:38 -0300

    create register button

--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -8,7 +8,18 @@ export default function Login() {
 
   const API_URL_ROOT = import.meta.env.VITE_API_URL
 
-  const API_URL = `${API_URL_ROOT}/get_user?email=`
+  const API_GET_USER = `${API_URL_ROOT}/get_user?email=`
+
+  const API_POST_USER = `${API_URL_ROOT}`
+
+
+  const todayPost = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // mês começa em 0
+    const day = String(today.getDate()).padStart(2, '0');
+    return `post_${year}-${month}-${day}`;
+  }
 
 
 
@@ -16,11 +27,11 @@ export default function Login() {
     const trimmedEmail = email.trim(); // Remove espaços antes e depois do email
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    
-    
+
+
     if (trimmedEmail && emailRegex.test(trimmedEmail)) {
-      const response = await fetch(`${API_URL}${trimmedEmail}`)
-      
+      const response = await fetch(`${API_GET_USER}${trimmedEmail}`)
+
       const jsonData = await response.json()
 
       if (jsonData.success) {
@@ -28,18 +39,41 @@ export default function Login() {
       } else {
         alert("Email não encontrado")
       }
-      
+
     } else {
       alert("Por favor, insira um e-mail válido.");
     }
   };
 
+
+  const handleRegister = async () => {
+    const trimmedEmail = email.trim(); // Remove espaços antes e depois do email
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+
+    if (trimmedEmail && emailRegex.test(trimmedEmail)) {
+      const response = await fetch(`${API_POST_USER}?email=${trimmedEmail}&id=${todayPost()}&utm_source=google&utm_medium=organic&utm_campaign=fall&utm_channel=web`)
+
+      const jsonResponse = await response.json()
+
+      if (jsonResponse.success) {
+        alert("Usuário cadastrado! Você será redirecionado para Home")
+        navigate(`/home/?email=${encodeURIComponent(trimmedEmail)}`);
+      }
+    } else {
+      alert("Por favor, insira um e-mail válido.");
+    }
+  }
+
+
+
   return (
     <main className="w-screen h-screen bg-tn_white flex justify-between">
-      
+
       {/* Seção da esquerda com informações e input */}
       <section className="flex flex-col justify-center items-start w-full h-full pl-20">
-        
+
         {/* Logo e título */}
         <header className="flex flex-col items-center justify-start gap-12">
           <img src="/assets/thenews_logo.png" className="w-40" alt="Logo do The News" />
@@ -48,7 +82,7 @@ export default function Login() {
 
         {/* Texto de boas-vindas e entrada de e-mail */}
         <section className="flex flex-col gap-12 pr-10">
-          
+
           {/* Mensagem principal */}
           <article className="flex flex-col gap-2">
             <h1 className="text-tn_black font-bold text-4xl">
@@ -58,16 +92,26 @@ export default function Login() {
               Nos conte qual o seu e-mail, e veja quantos pontos acumulou por ler diariamente!
             </p>
           </article>
-          
+
           {/* Campo de e-mail e botão de entrar */}
           <article className="flex flex-col gap-4">
             <InputMail email={email} setEmail={setEmail} />
-            <button
-              onClick={handleLogin}
-              className="bg-tn_yellow text-black font-bold text-lg px-6 py-2 rounded-lg hover:bg-yellow-600 transition"
-            >
-              Entrar
-            </button>
+            <div className="flex justify-center gap-20">
+              <button
+                onClick={handleRegister}
+                className="bg-amber-950 w-full text-tn_yellow font-bold text-lg px-6 py-2 rounded-lg hover:bg-amber-900 shadow-xl transition"
+              >
+                Cadastrar
+              </button>
+
+
+              <button
+                onClick={handleLogin}
+                className="bg-tn_yellow w-full text-black font-bold text-lg px-6 py-2 rounded-lg hover:bg-yellow-600 transition"
+              >
+                Entrar
+              </button>
+            </div>
             <p className="text-tn_yellow text-xl font-bold">
               Se destaque entre +2M de leitores.
             </p>
